2bda722f70dd19a4ddb0560d3abd338796d83d82
D M <gs9600@gmail.com>
2013-09-08 21:13:50 -0400

    Deleting skorksquine

--- skorksquine.rb
+++
@@ -1,3 +0,0 @@
-def method;"def method;;end;puts method()[0, 11] + 34.chr + method + 34.chr + method()[11, method.length-11]";end;
-
-puts method()[0, 11] + 34.chr + method + 34.chr + method()[11, method.length-11]
